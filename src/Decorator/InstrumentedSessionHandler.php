51730bb24decbfb7daaef618a311efe201bc6418
SyntaxPilot <nenglish0321@gmail.com>
2026-05-24 20:31:35 -0700

    add doc headers

--- src/Decorator/InstrumentedSessionHandler.php
+++ src/Decorator/InstrumentedSessionHandler.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Copyright (c) 2027 Nicholas English
+ *
+ * This file is licensed under the MIT License.
+ * See the LICENSE file in the project root for full license information.
+ */
 
 declare(strict_types=1);
 
